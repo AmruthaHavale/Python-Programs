e063240559d4130d8a93c3239e116a902088242c
Amrutha Havale <amruthakhavale@gmail.com>
2026-01-20 18:45:45 +0530

    Add files via upload

---
+++ KBC.py
@@ -0,0 +1,194 @@
+#THIS IS KBC USING IF STATEMENTS
+print("HELLO, WELCOME TO KAUN BANEGA KARODPATI")
+
+#question 1
+print("\nOKAY, LET'S START THE GAME  (ANSWER ONLY THE OPTIONS)")
+print("\nYOUR FIRST QUESTION FOR 1,000 RUPEES \n1)WHICH PLANET IS CALLED THE RED PLANET? \nYOUR OPTIONS ARE")
+print("a) EARTH \n b) MARS \n c) JUPITER \n d) SATURN")
+b = input("ANSWER: ")
+if(b == "b"):
+    print("\nCORRECT ANSWER.......!!!!!!!! \n\n YOU WON 1,000 RUPEES.....!!!!!!! ")
+else:
+    print("\nYOU FAILED....... \nYOU LOOSE ALL OF YOUR AMOUNT \nBETTER LUCK NEXT TIME......")
+    exit()
+
+#question 2
+print("YOUR NEXT QUESTION FOR RUPEES 2,000")
+print("\nWHO WROTE THE NATIONAL ANTHEM 'JANA GANA MANA'?")
+print("YOUR OPTIONS ARE")
+print("a) RABINDRANATH TAGORE \nb) MAHATMA GANDHI \nc) B.R.AMBEDKAR \nd) SUBHASH CHANDRA BOSE")
+a = input("ANSWER: ")
+if (a == "a"):
+    print("\nCORRECT ANSWER.... \nYOU WON 2,000 RUPEES.....")
+else:
+    print("\nOOPS YOU LOOSE...\nYOU LOOSE ALL YOUR MONEY")
+    exit()
+
+#question 3
+print("YOUR THIRD QUESTION FOR RUPEES 3,000 ")
+print("\nWhat is the capital of Australia?")
+print("YOUR OPTIONS ARE....")
+print("a)syndney \nb)canberra \nc)ottawa \nd)wellington")
+c = input("ANSWER: ")
+if (c == "b"):
+    print("\nCORRECT ANSWER.... \nYOU WON 3,000 RUPEES ")
+else:
+    print("\nOOPS WRONG ANSWER \nYOU LOOSE ALL YOU MONEY")
+    exit()
+
+# question 4
+print("YOUR FOURTH QUESTION FOR RUPEES 5,000")
+print("\nWHO IS THE FOUNDER OF MICROSOFT?")
+print("YOUR OPTIONS ARE")
+print("a) ELON MUSK \nb) SUNDER PICHAI \nc) STEVE JOBS \nd) BILL GATES")
+d = input("ANSWER: ")
+if (d == "d"):
+    print("\nCORRECT ANSWER \nYOU WON 5,000")
+else:
+    print("\nOOPS WRONG ANSWER\nYOU LOOSE ALL OF YOUR AMOUNT")
+    exit()
+
+# question 5
+print("YOUR NEXT QUESTION FOR RUPEES 10,000 (THIS IS A CHECKPOINT)")
+print("\nWhich Indian scientist is known as the 'Missile Man of India'?")
+print("Your options are:")
+print("a) APJ ABDUL KALAM \nb)HOMI JAHANGIR BABA \nc)ELON MUSK \nd)CV RAMAN")
+e = input("ANSWER:")
+if(e == "a"):
+    print("\nCorrect answer...\nYou won 10,000 rupees")
+else:
+    print("\nwrong answer...\n you loose all of your amount")
+    exit()
+
+# question 6
+print("Your next question for rupees 20,000")
+print("\nWhich is the largest desert in the world?")
+print("Your options are:")
+print("a) Antarctic polar desert \nb)sahara desert \nc)great australian desert \nc)gobi desert")
+f = input("ANSWER:")
+if(f == "a"):
+    print("\ncorrect answer....\nyou won 20,000")
+else:
+    print("\nwrong answer....you loose....\nyour net amount is 10,000")
+    exit()
+
+# question 7
+print("Your next question for rupees 40,000")
+print("\nWhat is the smallest unit of matter?")
+print("your options are: ")
+print("a)cells \nb) molecules \nc)dust \nd)atom")
+g = input("ANSWER: ")
+if (g == "d"):
+    print("\ncorrect answer...\nyou won 40,000")
+else:
+    print("\nwrong answer...\nyou loose\nyour net amount is 10,000")
+    exit()
+
+# question 8
+print("your next question for rupees 80,000")
+print("\nWhich Indian festival is known as the 'Festival of Lights'?")
+print("your options are:")
+print("a)sankranti \nb)diwali \nc)dasara \nd)christmas")
+h = input("ANSWER: ")
+if (h == "b"):
+    print("\ncorrect answer...\nyou won 80,000")
+else:
+    print("\nwrong answer...\nyou loose\nyour net amount is 10,000")
+    exit()
+
+# quetsion 9
+print("Your next question for rupees 1,60,000")
+print("\nWhich is the smallest continent in the world?")
+print("your options are:")
+print("a)India \nb)europe \nc)australia \nd)america ")
+i = input("ANSWER: ")
+if (i == "c"):
+    print("\ncorrect answer...\nyou won 1,60,000")
+else:
+    print("\nwrong answer...\nyou loose\nyour nest amount is 10,000")
+    exit()
+
+# question 10
+print("your next question for rupees 3,20,000(checkpoint)")
+print("\nWhat is the boiling point of water at standard atmospheric pressure(in degree celsius?")
+print("your options are:")
+print("a)27 \nb)39 \nc)100 \nd)1000")
+j = input("ANSWER:")
+if (j == "c"):
+    print("\ncorrect answer....\nyou won 3,20,000")
+else:
+    print("\nwrong answer...\nyou loose\nyour net amount is 10,000")
+    exit()
+
+# question 11
+print("your next question for rupees 6,40,000")
+print("\nWhich Indian state is known as the 'Land of Five Rivers'?")
+print("your options are")
+print("a)karnataka \nb)rajasthan \nc)punjab \nd)haryana")
+k = input("ANSWER: ")
+if (k == "c"):
+    print("\ncorrect answer..\nyou won 6,40,000")
+else:
+    print("\nworng answer...\nyou loose \nyour net amount is 3,20,000")
+    exit()
+
+# question 12
+print("your next questio  for rupees 12,50,000")
+print("\nWhat is the chemical symbol for gold?")
+print("your options are:")
+print("a)Au \nb)Ag \nc)O \nd)Fe")
+l = input("ANSWER: ")
+if(l == "a"):
+    print("\ncorrect answer...\nyou won 12,50,000")
+else:
+    print("\nwrong answer....\nyou loose  \nyour net amount is 3,20,000")
+    exit()
+
+# question 13
+print("your next question for rupees 25,00,000")
+print("\nWhich country is known as the Land of the Rising Sun?")
+print("your options are:")
+print("a)India \nb)china \nc)singapore \nd)japan")
+m = input("ANSWER:")
+if(m == "d"):
+    print("\ncorrect answer....\nyou won 25,00,000")
+else:
+    print("\nwrong answer...\nyou loose\nyour net amount is 3,20,000")
+    exit()
+
+# question 14
+print("your next question 50,00,000")
+print("\nWhich is the largest mammal in the world?")
+print("your options are:")
+print("a)blue whale \nb)elephant \nc)hippo \nd)tiger")
+n = input("ANSWER:")
+if(n == "a"):
+    print("\ncorrect answer...\nyou won 50,00,000")
+else:
+    print("\nwrong answer...\nyou loose\nyour net amount is 3,20,000")
+    exit()
+
+# question 15
+print("your next question for rupees one crore")
+print("\nWho is known as the 'Father of the Indian Constitution'?")
+print("your options ane:")
+print("a)gandhiji \nb)BR ambedkar \nc)Bhagat singh \nd)subhashchandra bose")
+o =input("ANSWER: ")
+if(o == "b"):
+    print("\ncorrect answer..\nyou won one crore")
+else:
+    print("\nwrong answer...\nyou loose\nyour net amount is 3,20,000")
+    exit()
+
+# quetsion 16
+print("your next question for 7 crore")
+print("\nwho is the father of our nation?")
+print("your options are:" )
+print("a)ganga \nb)yamuna \nc)nile \nd)krishna")
+p = input("ANSWER:")
+if(p == "c"):
+    print("\ncorrect option...\nyou won 7 crore \n congratulations\nthe end")
+else:
+    print("\nwrong answer..\nyou loose\nyour net amount is 3,20,000")
+    exit()
+
