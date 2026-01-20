120815bde93b5e7181466b7342be62b72f957ee4
Amrutha Havale <amruthakhavale@gmail.com>
2026-01-20 18:44:27 +0530

    Add files via upload

---
+++ CALCULATOR.py
@@ -0,0 +1,15 @@
+#this is a calculator which calculates given values
+a = int(input("ENTER THE FIRST NUMBER: "))
+c = input("ENTER THE OPERATION TO BE PERFORMED: ")
+b = int(input("ENTER THE SECOND NUMBER: "))
+if(c == "+"):
+    print("THE ADDITION OF ", a , " AND ", b , " IS ", a + b)
+elif(c == "-"):
+    print("THE SUBTRACTION OF ", a , " AND ", b , " IS ", a - b)
+elif(c == "*"):
+    print("THE SUBTRACTION OF ", a , " AND ", b , " IS ", a*b )
+elif(c == "/"):
+    print("THE SUBTRACTION OF ", a , " AND ", b , " IS ", a/b)
+print("THANK YOU")
+
+          
